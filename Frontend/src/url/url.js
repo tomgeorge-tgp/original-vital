0bf1e752b376367ab7343b60cd5970a3ed19cca7
tomgeorge_tgp <tomgeorgetgp@gmail.com>
2022-12-07 17:30:13 +0530

    the work flow and the relam of the bp sensor is done

---
+++ Frontend/src/url/url.js
@@ -0,0 +1,10 @@
+const rootUrl="http://localhost:5000";
+
+export const signupUrl="/user/signup";
+
+export const loginUrl="/user/login"; 
+
+export const fetchUserUrl="/user";
+
+export const sendBpSensorValue="/sensor/bpsensor";
+export const fetchSensorDataUrl="/sensor/bpfetchsensor";
