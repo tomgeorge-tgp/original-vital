927ce80e4210ae9170a4d338cf8c743ec30a1fcc
Tom George <tomgeorgetgp4187@gmail.com>
2022-12-07 17:12:04 +0530

    Delete Backend directory

--- Backend/Routes/appRoutes.js
+++
@@ -1,9 +0,0 @@
-
-import UserRoutes from './UserRoutes.js'
-import SensorRoutes from './SensorWebSocketRoutes.js'
-export default function(app)
-{
-    app.use('/user', UserRoutes);
-    // app.use('/sensor',SensorRoutes);
-   
-};
